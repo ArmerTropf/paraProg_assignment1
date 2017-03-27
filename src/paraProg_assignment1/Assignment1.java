e9331b572e0b6334783e4bf4f562dff1493c066f
ArmerTropf <mguenster@web.de>
2017-03-27 11:59:47 +0200

    bis aufgabe 1 fertig bis auf die default methode check()

--- src/paraProg_assignment1/Assignment1.java
+++ src/paraProg_assignment1/Assignment1.java
@@ -13,8 +13,8 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 
 public class Assignment1 {
-	private final static int INCREMENTERS = 500;
-	private final static int RUNS = 500000;
+	private final static int INCREMENTERS = 7;
+	private final static int RUNS = 5;
 	// private static long counter = 0;
 
 	protected static class  Incrementer implements Runnable {
@@ -45,9 +45,9 @@ public class Assignment1 {
 
 		CountDownLatch startLatch = new CountDownLatch(1);
 		CountDownLatch endLatch = new CountDownLatch(INCREMENTERS);
-		Thread[] Incrementers = new Thread[INCREMENTERS];
+		//Thread[] Incrementers = new Thread[INCREMENTERS];
 		
-		//ExecutorService myExCached = Executors.newCachedThreadPool();
+		ExecutorService myExCached = Executors.newCachedThreadPool();
 		//ExecutorService myExCached = Executors.newSingleThreadExecutor();
 		//ExecutorService myExCached = Executors.newFixedThreadPool(2);
 
@@ -55,63 +55,102 @@ public class Assignment1 {
 		for (int i = 0; i < INCREMENTERS; i++) {
 
 			//myExCached.submit(new Incrementer(startLatch, endLatch, new MyLong()));
-			Incrementers[i] = new Thread(new Incrementer(startLatch, endLatch, new MyLong()));
-			Incrementers[i].start();
+			myExCached.submit(new Incrementer(startLatch, endLatch, new MyLongAtomic()));
+			//myExCached.submit(new Incrementer(startLatch, endLatch, new MyLongAtomicModulo()));
+			
+			//Incrementers[i] = new Thread(new Incrementer(startLatch, endLatch, new MyLongAtomicModulo()));
+			//Incrementers[i].start();
+			
 		}
 		try {
-			
-
 			startLatch.countDown();
+			
 			endLatch.await();
 			long totalInc = RUNS * INCREMENTERS;
 			System.out.println("Finished after " + totalInc + " increments with counter = " + Incrementer.myLong_counter.get());
 		} catch (Exception e) {
 		}
-		//myExCached.shutdown();
+		myExCached.shutdown();
 
 	}
 }
 
 interface CounterInterface {
+
+	
 	long get();
-
 	long incrementAndGet();
-
-	void check(long desired);
+	default void check(long desired) {
+		
+	}
 }
 
-class MyLong extends MyLongAtomic implements CounterInterface {
+class MyLong implements CounterInterface {
 
 	private long myCounter = 0;
-			
+	
 	@Override
 	public long get() {
-		//return myCounter;
+		return myCounter;
+	}
+
+	@Override
+	public long incrementAndGet() {
+		this.myCounter++;
+		
+		return this.myCounter;	
+	}
+
+	@Override
+	public void check(long desired) {
+
+	}
+
+}
+
+class MyLongAtomic extends MyLong {
+	
+	
+	protected AtomicLong myAtomicLongCounter = new AtomicLong(0);
+	
+	@Override
+	public long get() {
+
 		return myAtomicLongCounter.get();
 	}
 
 	@Override
 	public long incrementAndGet() {
-		//this.myCounter++;
-		//return this.myCounter;
+		myAtomicLongCounter.incrementAndGet();
 		
-		this.myAtomicLongCounter.incrementAndGet();
 		return myAtomicLongCounter.get();
-		
-		
 	}
 
 	@Override
 	public void check(long desired) {
-		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+class MyLongAtomicModulo extends MyLongAtomic {
+	protected AtomicLong myAtomicLongModuloCounter = new AtomicLong(0);
+
+	@Override
+	public long get() {
+
+		return myAtomicLongModuloCounter.get();
+	}
+
+	@Override
+	public long incrementAndGet() {
+		myAtomicLongModuloCounter.set(myAtomicLongModuloCounter.incrementAndGet() % 16);
+		return myAtomicLongModuloCounter.get();
+	}
+
+	@Override
+	public void check(long desired) {
 
 	}
 
-}
-
-class MyLongAtomic {
-	
-	
-	protected AtomicLong myAtomicLongCounter = new AtomicLong(0);
-		
 }
